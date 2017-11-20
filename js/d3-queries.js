3f9152cdbd1bbb76c3bbac74bd48ccf1ba9f4c73
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2017-11-19 21:59:04 -0300

    add nodes and run queries

--- js/d3-queries.js
+++ js/d3-queries.js
@@ -1,138 +1,187 @@
-$(function () {
-    var w = 1200
-    var h = 800
+// ########## Data: Nodes and Links ##########
+
+nodes = [
+
+];
+
+links = [
+
+];
+
+graph = { nodes, links };
+
+w = 1200;
+h = 800;
+
+function drawForceCanvas() {
 
     // ########## SVG ##########
-    var svg = d3.select("#canvas")
+    svg = d3.select("#canvas")
         .attr("width", w)
         .attr("height", h);
 
-    var borderPath = svg.append("rect")
+    // ########## SVG Border##########
+    border = svg.append("rect")
         .attr("id", "canvas-border")
         .attr("x", 0)
         .attr("y", 0)
         .attr("height", h)
-        .attr("width", w)
+        .attr("width", w);
 
-    // ########## Data: Nodes and Links ##########
+    // ########## d3 Force Layout ##########
+    simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(w / 2, h / 2));
 
-    d3.json("data/data.json", function (error, data) {
-        if (error) throw error;
+    linkGroup = svg.append("g").attr("class", "link");
 
-        graph = data;
+    nodeGroup = svg.append("g").attr("class", "node");
+}
 
-        // ########## Links and nodes ##########
+function redraw() {
+    // ########## Links and nodes ##########
 
-        var link = svg.selectAll(".link")
-            .data(graph.links).enter()
-            .append("line")
-            .attr("class", "link");
+    // Update links
+    link = linkGroup
+        .selectAll("line")
+        .data(graph.links);
 
-        var node = svg.selectAll(".node")
-            .data(graph.nodes).enter()
-            .append("circle")
-            .attr("class", "node")
-            .attr("fill", function (d) { return d.color; })
-            .attr("stroke-width", 0)
-            .attr("r", 10)
+    // Enter links
+    linkEnter = link
+        .enter().append("line");
 
-        var label = svg.selectAll(".label")
-            .data(graph.nodes).enter()
-            .append("text")
-            .attr("class", "label")
-            .text(function (d) { return d.id; });
+    link = linkEnter
+        .merge(link);
 
-        // ########## Names ##########
+    // Exit any old links
+    link.exit().remove();
 
-        node.append("title")
-            .text(function (d) { return d.id; });
+    // Update the nodes
+    node = nodeGroup
+        .selectAll("circle")
+        .data(graph.nodes);
 
-        // ########## Force ##########
+    label = nodeGroup
+        .selectAll("text")
+        .data(graph.nodes);
 
-        var simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(w / 2, h / 2));
+    // Enter any new nodes
+    nodeEnter = node
+        .enter()
+        .append("circle")
+        .attr("fill", function (d) { return d.nodeType == "E" ? "purple" : "teal"; })
+        .attr("stroke-width", 0)
+        .attr("r", 10);
 
-        simulation.nodes(graph.nodes)
-            .on("tick", ticked);
+    labelEnter = label
+        .enter()
+        .append("text")
+        .text(function (d) { return d.id; });
 
-        simulation.force("link")
-            .links(graph.links);
+    node = nodeEnter.merge(node);
+    label = labelEnter.merge(label);
 
-        function ticked() {
-            link
-                .attr("x1", function (d) { return d.source.x; })
-                .attr("y1", function (d) { return d.source.y; })
-                .attr("x2", function (d) { return d.target.x; })
-                .attr("y2", function (d) { return d.target.y; });
+    // Exit any old nodes
+    node.exit().remove();
+    label.exit().remove();
 
-            node
-                .attr("cx", function (d) { return d.x; })
-                .attr("cy", function (d) { return d.y; });
+    // ########## Names ##########
 
-            label
-                .attr("x", function (d) { return d.x; })
-                .attr("y", function (d) { return d.y - 20; });
-        }
+    node.append("title")
+        .text(function (d) { return d.id; });
 
-        // ########## Toggle ##########
+    // ########## Force ##########
 
-        node.on("click", toggleSelected);
+    // Redefine and restart simulation
+    simulation.nodes(graph.nodes)
+        .on("tick", ticked);
 
-        function toggleSelected(d) {
-            d.isSelected = !d.isSelected;
-            d3.select(this).attr("stroke-width", d.isSelected ? 5 : 0);
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+
+        node
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
+
+        label
+            .attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y - 20; });
+    }
+
+    // ########## Toggle ##########
+
+    node.on("click", toggleSelected);
+
+    function toggleSelected(d) {
+        d.isSelected = !d.isSelected;
+        d3.select(this).attr("stroke-width", d.isSelected ? 5 : 0);
+    };
+
+    // ########## Drag and Drop ##########
+
+    node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    };
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    };
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        if (!d.isSelected) {
+            d.fx = null;
+            d.fy = null;
         };
+    };
 
-        // ########## Drag and Drop ##########
+    // ########## Zoom & Pan ##########
 
-        node.call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+    var zoom = d3.zoom()
+        .scaleExtent([0.5, 3])
+        .on("zoom", zoomed);
 
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        };
 
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        };
+    svg.call(zoom);
 
-        function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            if (!d.isSelected) {
-                d.fx = null;
-                d.fy = null;
-            };
-        };
+    function zoomed() {
+        node.attr("transform", d3.event.transform);
+        link.attr("transform", d3.event.transform);
+        label.attr("transform", d3.event.transform);
+    }
+}
 
-        // ########## Zoom & Pan ##########
-
-        var zoom = d3.zoom()
-            .scaleExtent([0.5, 3])
-            .on("zoom", zoomed);
-
-        svg.call(zoom);
-
-        function zoomed() {
-            node.attr("transform", d3.event.transform);
-            link.attr("transform", d3.event.transform);
-            label.attr("transform", d3.event.transform);
-        }
-
-    });
+$(function () {
+    drawForceCanvas();
+    redraw();
 });
 
+// ########## String ReplaceAll ##########
 String.prototype.replaceAll = function (search, replace) {
     if (replace === undefined) { return this.toString(); }
     return this.replace(new RegExp('[' + search + ']', 'g'), replace);
 };
 
+entityList = [];
+propertyList = [];
+
+// ########## Get Types ##########
 function getTypes(url, callback) {
     var query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT DISTINCT ?t WHERE { ?s rdf:type ?t } LIMIT 50";
     query = query.replaceAll(" ", "+").replaceAll("#", "%23");
@@ -142,12 +191,18 @@ function getTypes(url, callback) {
         dataType: "jsonp",
         url: queryUrl,
         success: function (_data) {
-            callback(_data.results.bindings);
+            entityList = _data.results.bindings;
+            callback();
         },
     });
 };
 
+
+// ########## Get Properties for Types ##########
 function getProperties(url, selected_type, callback) {
+
+    addNewEntity(selected_type);
+
     if (url && selected_type) {
         var query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT DISTINCT ?p WHERE { ?s rdf:type <" + selected_type + "> . ?s ?p ?o . } LIMIT 50";
         query = query.replaceAll(" ", "+").replaceAll("#", "%23");
@@ -157,13 +212,55 @@ function getProperties(url, selected_type, callback) {
             dataType: "jsonp",
             url: queryUrl,
             success: function (_data) {
-                callback(_data.results.bindings);
+                propertyList = _data.results.bindings;
+                callback();
             },
         });
     }
 };
 
-function createEntityDatalist(json) {
+function showResults(){
+    console.log(JSON.stringify(queryResults));
+}
+
+queryResults = [];
+
+function runQuery(url, callback) {
+    obj = 0;    
+    var query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT DISTINCT * WHERE { ";
+    console.log(query);
+    for (var i = 0; i < graph.nodes.length; i++) {
+        var n = graph.nodes[i];
+
+        if (n.nodeType == "E") {
+            query = query + "?s rdf:type <" + n.id + "> ."
+
+        }
+        if (n.nodeType == "P") {
+            obj = obj + 1;
+            query =  query +"?s <" + n.id + "> ?o"+obj+" ."
+        }
+    }
+    query = query + " }"
+
+    query = query.replaceAll(" ", "+").replaceAll("#", "%23");
+    var queryUrl = url + "?query=" + query + "&format=json";
+
+    $.ajax({
+        dataType: "jsonp",
+        url: queryUrl,
+        success: function (_data) {
+            queryResults = _data.results.bindings;
+            callback();
+        },
+    });
+}
+
+entityCount = 0;
+propertyCount = 0;
+
+// ########## Create DataList for Entities ##########
+function createEntityDatalist() {
 
     d3.select("#side-panel")
         .append("g")
@@ -171,16 +268,16 @@ function createEntityDatalist(json) {
 
     d3.select("#side-panel")
         .append("input")
-        .attr("id", "selectedEntityInput")
+        .attr("id", "entityInput" + entityCount)
         .attr("type", "text")
-        .attr("list", "dlist1")
+        .attr("list", "elist" + entityCount)
 
     datalist = d3.select("#side-panel")
         .append("datalist")
-        .attr("id", "dlist1")
+        .attr("id", "elist" + entityCount)
 
-    for (var i in json) {
-        var res = json[i];
+    for (var i in entityList) {
+        var res = entityList[i];
         keys = Object.keys(res);
         for (var j in keys) {
             datalist.append("option").attr("value", res[keys[j]].value);
@@ -191,35 +288,38 @@ function createEntityDatalist(json) {
         .append("input")
         .attr("type", "button")
         .attr("value", "Add")
-        .attr("onClick", "getProperties(endpointURL.value, selectedEntityInput.value, createPropertyDatalist)");
+        .attr("onClick", "getProperties(endpointURL.value, entityInput" + entityCount + ".value, createPropertyDatalist)");
 
     d3.select("#side-panel")
         .append("input")
         .attr("type", "button")
         .attr("value", "Run Query")
-        .attr("onClick", "");
+        .attr("onClick", "runQuery(endpointURL.value, showResults)");
 
     d3.select("#side-panel")
         .append("br");
-}
 
-function createPropertyDatalist(json) {
+    entityCount = entityCount + 1;
+}
+// ########## Create DataList for Properties ##########
+function createPropertyDatalist() {
+
     d3.select("#side-panel")
         .append("g")
         .text("Property:")
 
     d3.select("#side-panel")
         .append("input")
-        .attr("id", "selectedEntityInput")
+        .attr("id", "propertyInput" + propertyCount)
         .attr("type", "text")
-        .attr("list", "dlist2")
+        .attr("list", "plist" + propertyCount)
 
     datalist = d3.select("#side-panel")
         .append("datalist")
-        .attr("id", "dlist2")
+        .attr("id", "plist" + propertyCount)
 
-    for (var i in json) {
-        var res = json[i];
+    for (var i in propertyList) {
+        var res = propertyList[i];
         keys = Object.keys(res);
         for (var j in keys) {
             datalist.append("option").attr("value", res[keys[j]].value);
@@ -230,15 +330,42 @@ function createPropertyDatalist(json) {
         .append("input")
         .attr("type", "button")
         .attr("value", "Add")
-        .attr("onClick", "createPropertyDatalist(selectedEntityInput)");
+        .attr("onClick", "addPropertyCallback(propertyInput" + propertyCount + ".value)");
+    // .attr("onClick", "createPropertyDatalist(selectedEntityInput)");
 
     d3.select("#side-panel")
         .append("br");
+
+    propertyCount = propertyCount + 1;
 }
 
+function addPropertyCallback(propertyValue) {
+    addNewProperty(propertyValue);
+    createPropertyDatalist()
+}
+
+// ########## Load Endpoint ##########
 function loadEndpointURL(url) {
     if (url.value) {
         getTypes(url.value, createEntityDatalist);
     }
 };
 
+function addNewEntity(entityName) {
+    console.log("addNewEntity: " + entityName)
+    var n = { id: entityName, nodeType: "E" }
+    graph.nodes.push(n);
+
+    redraw();
+}
+
+function addNewProperty(propertyName) {
+    console.log("addNewProperty: " + propertyName);
+    var n = { id: propertyName, nodeType: "P" }
+    graph.nodes.push(n);
+    nodesLength = graph.nodes.length - 1;
+    var l = { source: graph.nodes[0], target: graph.nodes[nodesLength] }
+    graph.links.push(l);
+
+    redraw();
+}
